a5b5ddcdc6e2fd117716e7c64e6594f09df94142
unknown <diopsoda1812@gai.com>
2024-12-17 00:45:13 +0000

    changement3

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // Fonction principale pour afficher le message  
 function main() { 
-    echo "Hello, GitHub!";  
+    echo "Hello, gitbaleine!";  
     }  
     // Appel de la fonction principale  
     main(); 
